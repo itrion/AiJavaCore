4674c22c7c42a0aadc735df72bd587684099e0f2
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2013-06-03 00:25:38 +0100

    Added support to search metrics in MiniMax
    just for search time and nodes expanded because it has no sense for the other metrics

--- src/core/ai/searches/MiniMax.java
+++ src/core/ai/searches/MiniMax.java
@@ -4,6 +4,7 @@ import core.ai.Action;
 import core.ai.Heuristic;
 import core.ai.InformedState;
 import core.ai.PlayersEnviroment;
+import core.ai.SearchMetrics;
 import core.ai.State;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,10 +18,14 @@ public class MiniMax {
     private double beta;
     private double bestEvaluation;
     private InformedState bestMove;
+    private long initTime;
+    private long stopTime;
+    private static int expandedStates;
 
     public MiniMax(Heuristic heuristic, PlayersEnviroment enviroment) {
         this.heuristic = heuristic;
         this.enviroment = enviroment;
+        this.expandedStates = 0;
     }
 
     public State getNewState() {
@@ -28,8 +33,10 @@ public class MiniMax {
     }
 
     public InformedState searchNextState(InformedState state, int maxDepth) {
+        setStartTime();
         initSearchParameters();
         for (InformedState child : getChilds(state)) {
+            expandedStates++;
             if (bestMove == null) bestMove = child;
             alpha = Math.max(alpha, minimax(child, maxDepth, alpha, beta));
             if (alpha > bestEvaluation) {
@@ -37,6 +44,7 @@ public class MiniMax {
                 bestEvaluation = alpha;
             }
         }
+        setStopTime();
         return bestMove;
     }
 
@@ -66,6 +74,7 @@ public class MiniMax {
 
     private double executeMaxTurn(InformedState state, int maxDepth, double alpha, double beta) {
         for (InformedState child : getChilds(state)) {
+            expandedStates++;
             alpha = Math.max(alpha, minimax(child, maxDepth - 1, alpha, beta));
             if (alpha >= beta) return beta;
         }
@@ -85,5 +94,20 @@ public class MiniMax {
         for (Action action : (List<Action>) enviroment.getApplicableActions(state))
             childs.add((InformedState) action.execute(state));
         return childs;
+    }
+
+    private void setStartTime() {
+        this.initTime = System.currentTimeMillis();
+    }
+
+    private void setStopTime() {
+        this.stopTime = System.currentTimeMillis();
+    }
+
+    public SearchMetrics getSearchMetrics() {
+        SearchMetrics metrics = new SearchMetrics();
+        metrics.setExpandedStates(expandedStates);
+        metrics.setSearchTime(stopTime - initTime);
+        return metrics;
     }
 }
